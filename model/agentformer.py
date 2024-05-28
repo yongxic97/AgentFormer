f6f4a5cc8cdaee6b6efa608058d49d9568f45339
yongxi <cyxsustech@gmail.com>
2024-05-28 13:35:35 +0200

    finish baseline test 0523_0000

--- model/agentformer.py
+++ model/agentformer.py
@@ -232,7 +232,7 @@ class FutureEncoder(nn.Module): # approximate posterior
         )
 
         self.csv_newstamp = str(ctx['epochs']) + ".csv"
-        self.this_run_info = '0523_0103_take1'
+        self.this_run_info = '0523_0000_take3'
 
     def forward(self, data, reparam=True):
         traj_in = []
@@ -376,7 +376,7 @@ class FutureDecoder(nn.Module):
         self.copy_future_encoder = future_encoder
 
         self.csv_newstamp = str(ctx['epochs']) + ".csv"
-        self.this_run_info = '0523_0103_take1'
+        self.this_run_info = '0523_0000_take3'
 
     def regen_posterior(self, data, pred_vel, pred_sn):
         traj_in = []
